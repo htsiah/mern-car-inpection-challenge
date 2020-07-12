422c8517166ad25c6f37bb231a64b7dd8dfe09d9
Simon Siah <htsiah@hotmail.com>
2020-07-12 16:46:02 +0800

    change folder structure

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect(`mongodb://localhost:27017/mern-car-inpection-challenge`, {
+  .connect(`mongodb+srv://carsome:VGGEYhC7BAtM8ad@cluster0.ttpav.mongodb.net/MERN-CAR-INPECTION-CHALLENGE?retryWrites=true&w=majority`, {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
